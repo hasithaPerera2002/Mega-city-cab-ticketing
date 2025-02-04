7d36c44f5fdc3ab252713435007a116c84fadfa8
hasithaPerera2002 <hasithaperera2017@gmail.com>
2025-02-04 23:03:35 +0530

    MEGA-CITY : Create user dto

--- mega-city/src/main/java/dto/UserDTO.java
+++ mega-city/src/main/java/dto/UserDTO.java
@@ -1,4 +1,19 @@
 package dto;
 
+import util.enums.RoleType;
+
+import java.util.UUID;
+
 public class UserDTO {
+    private UUID user_id;
+    private String username;
+    private String password_hash;
+    private String email;
+    private String phone_number;
+    private String address;
+    private RoleType role;
+    private String nic;
+    private String front_image;
+    private String back_image;
+
 }
---
+++ mega-city/src/main/java/util/enums/RoleType.java
@@ -0,0 +1,6 @@
+package util.enums;
+
+public enum RoleType {
+    ADMIN,
+    USER
+}
