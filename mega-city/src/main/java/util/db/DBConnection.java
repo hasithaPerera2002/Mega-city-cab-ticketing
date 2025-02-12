c9d2b035d1a45a8105090b975c533942ab4f2223
hasithaPerera2002 <hasithaperera2017@gmail.com>
2025-02-12 19:49:26 +0530

    MEGA-CITY : Write test cases for db connection

--- mega-city/src/main/java/util/db/DBConnection.java
+++ mega-city/src/main/java/util/db/DBConnection.java
@@ -1,7 +1,8 @@
 package util.db;
 
+
+
 import org.slf4j.Logger;
-import util.logger.GlobalLogger;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -10,20 +11,20 @@ import java.sql.SQLException;
 public class DBConnection {
     private static DBConnection instance ;
     private final Connection connection;
-
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(DBConnection.class);
     private static final String URL = "jdbc:postgresql://localhost:5432/megacity";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "postgres";
     private static final String DRIVER = "org.postgresql.Driver";
-    Logger LOGGER = GlobalLogger.getLogger();
+
 
     private DBConnection() throws SQLException {
         try {
             Class.forName(DRIVER);
             connection = java.sql.DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            LOGGER.info("Connected to the database");
+            log.info("Connected to the database");
         } catch (ClassNotFoundException | SQLException e) {
-            LOGGER.error("Error occurred while connecting to the database", e);
+            log.error("Error occurred while connecting to the database", e);
             throw new SQLException("Error occurred while connecting to the database", e);
         }
     }
@@ -31,11 +32,22 @@ public class DBConnection {
     public static DBConnection getInstance() throws SQLException {
         if (instance == null) {
             instance = new DBConnection();
+        }else if (instance.getConnection().isClosed()){
+            instance = new DBConnection();
         }
         return instance;
     }
-
     public Connection getConnection() {
+        log.info("Returning the connection to database");
+        try {
+            if (connection != null && !connection.isClosed()) {
+                log.info("Connection is valid");
+            } else {
+                log.warn("Connection is closed or invalid");
+            }
+        } catch (SQLException e) {
+            log.error("Error while checking connection status", e);
+        }
         return connection;
     }
 }
